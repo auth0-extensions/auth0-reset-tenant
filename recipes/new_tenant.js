d5632dab70a18efb106a62448ef285b158dfc55c
robert rexford <robert.rexford@gmail.com>
2016-09-08 14:51:32 -0700

    Resolves #1

--- recipes/new_tenant.js
+++ recipes/new_tenant.js
@@ -1,21 +1,25 @@
 import { createEntity } from '../lib/auth0';
 
-const CLIENT_NAME = 'Default App';
-const DB_CONNECTION_NAME = 'Username-Password-Authentication';
-
 export const name = 'New Tenant';
-export const description = `Creates a '${CLIENT_NAME}' client and a '${DB_CONNECTION_NAME}' connection`;
+export const description = 'Creates all the elements in a new Auth0 Tenant';
 
 export const run = (accessTokens) =>
   // Create a regular web application
   createEntity('Client', 'client_id', '/api/v2/clients', accessTokens.v2, {
-    name: CLIENT_NAME
+    name: 'Default App'
   })
     // Create DB connection
     .then(client =>
-      createEntity('Connection', 'id', '/api/v2/connections', accessTokens.v2, {
-        name: DB_CONNECTION_NAME,
-        strategy: 'auth0',
-        enabled_clients: [ client.client_id ]
-      })
+      Promise.all([
+        createEntity('Connection', 'id', '/api/v2/connections', accessTokens.v2, {
+          name: 'Username-Password-Authentication',
+          strategy: 'auth0',
+          enabled_clients: [ client.client_id ]
+        }),
+        createEntity('Connection', 'id', '/api/v2/connections', accessTokens.v2, {
+          name: 'google-oauth2',
+          strategy: 'google-oauth2',
+          enabled_clients: [ client.client_id ]
+        })
+      ])
     );
