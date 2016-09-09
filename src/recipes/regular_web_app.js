ebebe2581c947b55a70728aea1cf5d8b09013f27
Peter Stromquist <peter@twistedstream.com>
2016-09-09 06:44:42 -0500

    Remove the API_CLIENT_ID enabled client
    from the DB connection in the regular_web_app recipe
    Resolves #2

--- src/recipes/regular_web_app.js
+++ src/recipes/regular_web_app.js
@@ -1,4 +1,4 @@
-import { createEntity } from '../lib/auth0';
+import { createEntity, updateEntity } from '../lib/auth0';
 
 const DB_CONNECTION_NAME = 'Sweet-App-Users';
 
@@ -19,17 +19,23 @@ export const run = (accessTokens) =>
         strategy: 'auth0',
         enabled_clients: [ client.client_id, process.env.API_CLIENT_ID ]
       })
-    )
-    // Add a single user
-    .then(() =>
-      createEntity('User', 'user_id', '/api/v2/users', accessTokens.v2, {
-        connection: DB_CONNECTION_NAME,
-        email: 'foo@example.com',
-        password: 'pw',
-        user_metadata: {
-          given_name: 'Foo',
-          family_name: 'Bar',
-          name: 'Foo Bar'
-        }
-      })
+      // Add a single user
+      .then(connection =>
+        createEntity('User', 'user_id', '/api/v2/users', accessTokens.v2, {
+          connection: DB_CONNECTION_NAME,
+          email: 'foo@example.com',
+          password: 'pw',
+          user_metadata: {
+            given_name: 'Foo',
+            family_name: 'Bar',
+            name: 'Foo Bar'
+          }
+        })
+        // remove API_CLIENT_ID from the connection's enabled clients
+        .then(() =>
+          updateEntity('Connection', connection.id, '/api/v2/connections', accessTokens.v2, {
+            enabled_clients: [ client.client_id ]
+          })
+        )
+      )
     );
