eb2d18d014d15adc65598f47dee1b61af5533d73
Peter Stromquist <peter@twistedstream.com>
2016-12-08 12:09:59 -0600

    Use strategy pattern for loading recipies
    currentDirectoryStrategy: load internal recipes from src/recipes
    commandLineImportStrategy: load strategies from files passed via extra command line args

--- src/recipes/index.js
+++ src/recipes/index.js
@@ -3,19 +3,33 @@ import path from 'path';
 
 const DEFAULT_ID = 'clear';
 
-export default () => new Promise((resolve, reject) =>
-  fs.readdir(__dirname, (err, files) => {
-    if (err) return reject(err);
+const currentDirectoryStrategy = () => 
+  new Promise((resolve, reject) =>
+    fs.readdir(__dirname, (err, files) => {
+      if (err) return reject(err);
 
-    const all = files
-      .filter(f => f.endsWith('.js') && f !== 'index.js')
-      .map(f => Object.assign(
-        { id: /^(.*)\.js$/.exec(f)[1] },
-        require(path.join(__dirname, f))));
+      const recipes = files
+        .filter(f => f.endsWith('.js') && f !== 'index.js')
+        .map(f => Object.assign(
+          { id: /^(.*)\.js$/.exec(f)[1] },
+          require(path.join(__dirname, f))));
 
-    // return array with the default recipe always first
-    const defaultRecipe = all.find(r => r.id === DEFAULT_ID);
-    const otherRecipes = all.filter(r => r.id !== DEFAULT_ID);
-    return resolve([ defaultRecipe ].concat(otherRecipes));
-  })
-);
+      resolve(recipes);
+    }));
+
+export default strategies => {
+  // add current directory strategy to passed list
+  strategies = strategies || [];
+  strategies.push(currentDirectoryStrategy);
+
+  // run strategies
+  return Promise.all(strategies.map(s => s()))
+    .then(results => {
+      const all = results.reduce((p, c) => p.concat(c), []);
+
+      // return all recipes with the default recipe always first
+      const defaultRecipe = all.find(r => r.id === DEFAULT_ID);
+      const otherRecipes = all.filter(r => r.id !== DEFAULT_ID);
+      return [ defaultRecipe ].concat(otherRecipes);
+    });
+};
